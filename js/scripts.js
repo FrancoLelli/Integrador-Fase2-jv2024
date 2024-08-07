32628a9c19ed4a8cda59dbe3cfc32769817455ce
FrancoLelli <francogastonlelli@gmail.com>
2024-08-06 21:02:43 -0300

    Se termino la fase 2

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,27 @@ document
     console.log(`Valor de la Propiedad: $${valorPropiedad}`);
     console.log(`Cotización: $${cotizacion.toFixed(2)}`);
   });
+
+function fetchData() {
+  fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => {
+      const resultContainer = document.getElementById('api-result');
+      resultContainer.innerHTML = '';
+
+      data.results.forEach((character) => {
+        const characterElement = document.createElement('div');
+        characterElement.classList.add('character');
+
+        characterElement.innerHTML = `
+          <h2>${character.name}</h2>
+          <img src="${character.image}" alt="${character.name}">
+          <p>Status: ${character.status}</p>
+          <p>Species: ${character.species}</p>
+        `;
+
+        resultContainer.appendChild(characterElement);
+      });
+    })
+    .catch((error) => console.error('Error fetching data:', error));
+}
